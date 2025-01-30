21cbe3cafe12618bed117abba73d57b4dc9c76e1
gursehajQB <gursehaj.singh@quadbtech.com>
2025-01-30 17:59:26 +0530

    changes in add object function

--- tests/Local/start.sh
+++ tests/Local/start.sh
@@ -14,3 +14,5 @@ dfx canister update-settings Growntown_AdminFrontend --add-controller 5poib-uaar
 dfx canister update-settings Growntown_Frontend --add-controller 5poib-uaar7-uhwx6-hq2iv-r3nun-lx5kz-gkams-nvxce-szojg-sipiu-4ae
 
 dfx canister update-settings icrc2_token_canister --add-controller 5poib-uaar7-uhwx6-hq2iv-r3nun-lx5kz-gkams-nvxce-szojg-sipiu-4ae
+
+dfx canister update-settings Growntown_Backend --add-controller 4kxuh-gb7ut-zf7uz-3z7s3-bmu2x-46huq-km4ea-baabl-vqcst-sxxbr-nqe
